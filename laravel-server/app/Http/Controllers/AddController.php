08f8b8e671e575cd3b526b5f88429094f6bbe1f7
Nour Mshawrab <mshawrabnour@gmail.com>
2022-06-10 00:57:10 +0300

    Implementing Add functions

--- laravel-server/app/Http/Controllers/AddController.php
+++ laravel-server/app/Http/Controllers/AddController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Product;
 
-class AddController extends Controller
-{
-    //
-}
+
+class AddController extends Controller{
+    
+    public function addProduct(Request $request){
+        $product = new Product;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->save();
+        
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
+
+
+    public function addCategory(Request $request){
+        $category = new Category;
+        $category->name = $request->name;
+        $category->save();
+        
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
+
+    };
--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -3,17 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\AddController;
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix("admin")->group(function(){
+    Route::post("/add_product", [AddController::class, "addProduct"]);
+    Route::post("/add_category", [AddController::class, "addCategory"]);
+});
+
+Route::prefix("customer")->group(function(){
+    Route::get("/get_products", [GetController::class, "getProducts"]);
+    
+})
