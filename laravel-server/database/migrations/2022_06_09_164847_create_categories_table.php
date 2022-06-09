2915d1d9b3655f112e16dafa453665bc967445d7
Nour Mshawrab <mshawrabnour@gmail.com>
2022-06-09 21:12:40 +0300

    Initialising repository

---
+++ laravel-server/database/migrations/2022_06_09_164847_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("Women's Fashion");
+            $table->string("Men's Fashion");
+            $table->string("Kids");
+            $table->string("Beauty & Health");
+            $table->string("Electronics");
+            $table->string("Home & Appliances");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
