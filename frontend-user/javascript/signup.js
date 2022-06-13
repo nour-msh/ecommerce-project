dc154eac92f3cddafc505da15a1683c40a38d014
Nour Mshawrab <mshawrabnour@gmail.com>
2022-06-13 22:50:24 +0300

    Changing signup/login structure

--- frontend-user/javascript/signup.js
+++ frontend-user/javascript/signup.js
@@ -1,6 +1,6 @@
 
     const bttn=document.getElementById("signupButton");
-    const name=document.getElementById("name");
+    const Name=document.getElementById("name");
     const number= document.getElementById("number");
     const email=document.getElementById("email");
     const password=document.getElementById("password");
@@ -8,15 +8,17 @@
     bttn.addEventListener("click",SignUp())
     function SignUp(){
         let data = new FormData();
-        data.append('name', name.value);
+        data.append('name', Name.value);
         data.append('number', number.value);
         data.append('email', email.value);
         data.append('password', password.value);
 
+        const token = JSON.parse(sessionStorage.getItem('data'));
+        // const token = user.data.id;
+
         axios.post('http://127.0.0.1:8000/api/customer/register',
-        {data: data,},
-        {headers:{'authorisation' : `token= $token`
-    }})       
+        data,
+        {headers: { Authorization: `Bearer ${token}`}} )       
         .then(function (response) {
             console.log(response);
             console.log("hi");
